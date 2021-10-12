8497cad1e6eec4b9fff2bc51541f3215cae53d32
Giash ITH <89529541+Giash1@users.noreply.github.com>
2021-10-12 12:33:21 +0200

    Add files via upload

---
+++ kod.js
@@ -0,0 +1,117 @@
+//Password validation - Without RegExp//
+
+function validdatePassword(arr){
+let newPassword=("");
+newPassword;
+if ( checkLength(password) && checkUpperCase(password) && checkLowerCase
+ 
+(password) && checkNumbers(password) && specialCharacter(password) ) {
+         System.out.print("Password accepted");
+         valid=true;
+      }
+      else {
+         System.out.print("Invalid password, try again");
+         valid=false;
+
+      }
+
+}
+
+   function length(password){
+      for(let i=0; i>=password.length()>=8 && i<=password.length()<=50; ){
+      length=true;
+   }
+   
+    {
+         length=false;
+      
+   }
+      return length;
+
+   }
+
+   function upperCase(password){
+      let j=uppercase[j];
+      for(let j=1; j<=password.length()<=50; ){
+      upperCase true;
+      }
+      else {
+         upperCase false;
+      }
+return upperCase;
+   }
+
+   function lowerCase(password){
+      let k=lowerCase[k];
+      for(let k=1; k<=password.length()<=50; ){
+         lowerCase true;
+      }
+      else{
+         lowerCase false;
+      }
+      return lowerCase;
+      }
+
+
+   function number(passwor){
+      let l=number[l];
+      for(let l=1;l<=password.length()<=50;){
+         number true;
+      }
+      else{
+       number false;
+      }
+      return number;
+   }
+   
+   function specialCharacter(password){
+   let m=specialCharacter[m];
+   for(let m=1;m<=password.length()<=50;){
+      specialCharacter[m]==specialCharacterLista[" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+  specialCharacter true;
+   }
+   else{
+      specialCharacter false;
+   }
+   return specialCharacter;
+   
+
+}
+return newPassword,
+
+
+
+
+   validatePassword("Abcd123!");
+
+
+
+
+
+
+// order sum
+
+function orderTotalt([]){
+   let;
+   {
+      name: Gurka,
+ amount: 2,
+ price: 10;
+   },
+   {
+      name: vattenmelon,
+ amount: 1,
+ price: 30;
+   },
+   {
+      name: kokosnöt,
+ amount: 2,
+ price: 25;
+   },
+}
+orderTotalt([ {name: 'Kaffe', amount: 1, price: 25},
+{name: 'Muffin', amount: 1, price: 30}]);
+
+
+
+
